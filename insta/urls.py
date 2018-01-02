8eb6b55f51f57a5e59c5e09fcfd1d877b91d91a6
Lizeth Buenrostro <lbuenrostro@basecampcodingacademy.org>
2018-01-02 16:05:20 -0600

    in urls a added two paths, one for the glass filter & hte other for the gray filter. more can be added once a I create a new usuable filter

--- insta/urls.py
+++ insta/urls.py
@@ -5,4 +5,7 @@ app_name = 'insta'
 urlpatterns = [
     path('add/', views.model_form_upload, name="base"),
     path('feed/', views.show_feed, name="feed"),
+    # path('delete/<image_id>', views.show_filter, name='feed'),
+    path('filter/glass/<image_id>', views.glass_filter, name='glass'),
+    path('filter/grey/<image_id>', views.grey_filter, name='grey')
 ]
