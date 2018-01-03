98bdebcf2cdcf4f540b13a215b0161010d4ac5d9
Lizeth Buenrostro <lbuenrostro@basecampcodingacademy.org>
2018-01-03 12:48:00 -0600

    image_url function that will let each image if deleted from feed also be deleted from insta/images the apps picture storage this will attribute with the delete button I have created

--- insta/models.py
+++ insta/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class DocumentForm(models.Model):
     description = models.CharField(max_length=255, blank=True)
     photo = models.ImageField(upload_to='insta/static/insta/images')
+
+    def image_url(self):
+        return self.photo.url[len('insta/images/'):]
