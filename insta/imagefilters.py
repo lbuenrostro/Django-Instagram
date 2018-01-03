af3b3946c5878316ec5d7d74c3257c0944340449
Lizeth Buenrostro <lbuenrostro@basecampcodingacademy.org>
2018-01-03 15:16:27 -0600

    Fixed rain_fall function to where it can blend in perfectly as a filter, also made a new filter function that will resemble fire

--- insta/imagefilters.py
+++ insta/imagefilters.py
@@ -66,10 +66,12 @@ def convert_grayscale(path):
 
 
 #############################################################################################
-def rain_fall(path):
+def rain_fall(path, side=800):
     rain = Image.open(
         '/home/basecamp/Projects/Daily_Exercise/December/Django-insta/insta/static/insta/images/rainfall.jpg'
     ).convert('RGB')
+    rain = resizeimage.resize_cover(rain, [side, side], validate=False)
+    rain = rain.convert('RGB').quantize(64).convert('RGB')
 
     image = Image.open(path).convert('RGB')
     w, h = image.size
@@ -77,32 +79,76 @@ def rain_fall(path):
     box = ((w - s) // 2, (h - s) // 2, (s + w) // 2, (s + h) // 2)
     image = image.crop(box=box)
 
-    image = resizeimage.resize_cover(image, [800, 800], validate=False)
-    image = image.convert('RGB')
+    image = resizeimage.resize_cover(image, [side, side], validate=False)
+    image = image.convert('RGB').quantize(255).convert('RGB')
 
     # get the new size
     w, h = image.size
     pixel_data = list(image.getdata())
 
     new_data = []
-    for t in pixel_data:
+    n = len(pixel_data)
+    for i in range(n):
+        t = pixel_data[i]
         r, g, b = t
-        new_data.append((r, g, 50))
+        new_data.append((max(0, int(r - 255 * (i**2 / n**2))), max(
+            0, int(r - 255 * (i**2 / n**2))), b))
 
     image = Image.new('RGB', (w, h))
     image.putdata(new_data)
-    image = resizeimage.resize_cover(image, [800, 800], validate=False)
+    image = resizeimage.resize_cover(image, [side, side], validate=False)
+    image = image.convert('RGB')
 
-    finalimage = Image.blend(image, rain, .3)
+    finalimage = Image.blend(image, rain, .4)
+    finalimage.save(path)
+    return True
+
+
+#############################################################################################
+def fire_all(path, side=800):
+    fire = Image.open(
+        '/home/basecamp/Projects/Daily_Exercise/December/Django-insta/insta/static/insta/images/fire.jpg'
+    ).convert('RGB')
+    fire = resizeimage.resize_cover(fire, [side, side], validate=False)
+    fire = fire.convert('RGB').quantize(64).convert('RGB')
+
+    image = Image.open(path).convert('RGB')
+    w, h = image.size
+    s = min([w, h])
+    box = ((w - s) // 2, (h - s) // 2, (s + w) // 2, (s + h) // 2)
+    image = image.crop(box=box)
+
+    image = resizeimage.resize_cover(image, [side, side], validate=False)
+    image = image.convert('RGB').quantize(255).convert('RGB')
+
+    # get the new size
+    w, h = image.size
+    pixel_data = list(image.getdata())
+
+    new_data = []
+    n = len(pixel_data)
+    for i in range(n):
+        t = pixel_data[i]
+        r, g, b = t
+        # new_data.append((max(0, int(r - 255 * (i**2 / n**2))), max(
+        #     0, int(r - 255 * (i**2 / n**2))), b))
+        new_data.append((r, max(0, int(r - 255 * (i**2 / n**2))), max(
+            0, int(b - 255 * (i**2 / n**2)))))
+
+    image = Image.new('RGB', (w, h))
+    image.putdata(new_data)
+    image = resizeimage.resize_cover(image, [side, side], validate=False)
+    image = image.convert('RGB')
+
+    finalimage = Image.blend(image, fire, .4)
     finalimage.save(path)
     return True
 
 
 def main():
-    image = Image.open(
-        '/home/basecamp/Projects/Daily_Exercise/December/Django-insta/insta/static/insta/images/Bowling.png'
-    ).convert('RGB')
-    grey = convert_grayscale(image)
+    grey = rain_fall(
+        '/home/basecamp/Projects/Daily_Exercise/December/Django-insta/insta/static/insta/images/pug_zWBzUvm.jpg'
+    )
     grey.show()
 
 
