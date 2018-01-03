227a4014775ac4ef53293f788896d0332e5201be
Lizeth Buenrostro <lbuenrostro@basecampcodingacademy.org>
2018-01-03 12:44:17 -0600

    made a rain filter, however still doesn't work, I need a image_id path

--- insta/imagefilters.py
+++ insta/imagefilters.py
@@ -65,6 +65,39 @@ def convert_grayscale(path):
     return True
 
 
+#############################################################################################
+def rain_fall(path):
+    rain = Image.open(
+        '/home/basecamp/Projects/Daily_Exercise/December/Django-insta/insta/static/insta/images/rainfall.jpg'
+    ).convert('RGB')
+
+    image = Image.open(path).convert('RGB')
+    w, h = image.size
+    s = min([w, h])
+    box = ((w - s) // 2, (h - s) // 2, (s + w) // 2, (s + h) // 2)
+    image = image.crop(box=box)
+
+    image = resizeimage.resize_cover(image, [800, 800], validate=False)
+    image = image.convert('RGB')
+
+    # get the new size
+    w, h = image.size
+    pixel_data = list(image.getdata())
+
+    new_data = []
+    for t in pixel_data:
+        r, g, b = t
+        new_data.append((r, g, 50))
+
+    image = Image.new('RGB', (w, h))
+    image.putdata(new_data)
+    image = resizeimage.resize_cover(image, [800, 800], validate=False)
+
+    finalimage = Image.blend(image, rain, .3)
+    finalimage.save(path)
+    return True
+
+
 def main():
     image = Image.open(
         '/home/basecamp/Projects/Daily_Exercise/December/Django-insta/insta/static/insta/images/Bowling.png'
