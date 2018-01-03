9cafa34bf3c972a36578862806bf474b90a55b3f
Lizeth Buenrostro <lbuenrostro@basecampcodingacademy.org>
2018-01-03 15:17:45 -0600

    Made a path for the fire filter

--- insta/urls.py
+++ insta/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     path('add/', views.model_form_upload, name="base"),
     path('feed/', views.show_feed, name="feed"),
     path('filter/glass/<image_id>', views.glass_filter, name='glass'),
-    path('filter/grey/<image_id>', views.grey_filter, name='grey')
+    path('filter/grey/<image_id>', views.grey_filter, name='grey'),
+    path('filter/rain/<image_id>', views.rain_filter, name='rain'),
+    path('filter/fire/<image_id>', views.fire_filter, name='fire'),
+    path('delete/<image_id>', views.remove, name='delete'),
 ]
