760274b4662a02df0027a0485cc27b546d9d7a26
Lizeth Buenrostro <lbuenrostro@basecampcodingacademy.org>
2018-01-03 16:36:24 -0600

    for the filter Form to work i made a class that controls how the filter is display and better for users

--- insta/views.py
+++ insta/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render, redirect
 from django.conf import settings
 from django.core.files.storage import FileSystemStorage
 from django.http import HttpResponseRedirect, HttpResponse
-from .forms import DocumentForm
+from .forms import DocumentForm, FiltersForm
 from insta import models
 from PIL import Image, ImageFilter
 from insta.imagefilters import convert_grayscale, broken_glass, rain_fall, fire_all
+from django.views import View
 
 
 # just a little test function when things go awry
@@ -62,3 +63,22 @@ def fire_filter(request, image_id):
 def remove(request, image_id):
     models.DocumentForm.objects.get(id=image_id).delete()
     return redirect('insta:feed')
+
+
+class Apply_Filter(View):
+    def get_pic(self, request, image_id):
+        form = FiltersForm()
+        path = 'insta/static/', models.DocumentForm.objects.get(
+            id=image_id).image_url()
+        return render(request, 'insta/feed.html', {'form': form})
+
+    def post_pic(self, request, image_id):
+        form = FiltersForm(request.POST)
+        path = 'insta/static/' + models.DocumentForm.objects.get(
+            id=image_id).image_url()
+        image = Image.open(path)
+        if form.is_valid():
+            f = form.apply_filter()
+            image.convert('RGB').Filter(f).save(path)
+            return redirect('insta:feed')
+        return render(request, 'insta/filter-image.html', {'form': form})
