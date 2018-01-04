39a89fb289a283b62e8393b5bd6650d13ff4afdf
Lizeth Buenrostro <lbuenrostro@basecampcodingacademy.org>
2018-01-04 09:43:08 -0600

    Fixed the class for filters some filters were missing and misplaced

--- insta/forms.py
+++ insta/forms.py
@@ -10,32 +10,24 @@ class DocumentForm(forms.ModelForm):
 
 
 class FiltersForm(forms.Form):
-    choices = [
-        ('', ''),
-        ('broken_glass', 'Broken Glass'),
-        ('convert_grayscale', 'Grey'),
-        ('BLUR', 'Blur'),
-        ('CONTOUR', 'Contour'),
-        ('DETAIL', 'Detail'),
-        ('EDGE_ENHANCE_MORE', 'Extra Edge-Enhance'),
-        ('EMBOSS', 'Emboss'),
-        ('FIND_EDGES', 'Find Edges'),
-        ('SMOOTH', 'Smooth'),
-        ('SMOOTH_MORE', 'Extra Smooth'),
-        ('SHARPEN', 'Sharpen'),
-    ]
-    filters = forms.ChoiceField(choices=choices)
+    filters_choice = [('', ''), ('BLUR', 'BLUR'), ('CONTOUR', 'CONTOUR'),
+                      ('DETAIL', 'DETAIL'), ('EDGE_ENHANCE', 'EDGE_ENHANCE'),
+                      ('EDGE_ENHANCE_MORE', 'EDGE_ENHANCE_MORE'), ('EMBOSS',
+                                                                   'EMBOSS'),
+                      ('FIND_EDGES', 'FIND_EDGES'), ('SMOOTH', 'SMOOTH'),
+                      ('SMOOTH_MORE', 'SMOOTH_MORE'), ('SHARPEN', 'SHARPEN')]
+    filters = forms.ChoiceField(choices=filters_choice)
 
     def apply_filter(self):
         return {
-            'Broken Glass': broken_glass,
-            'Grey': convert_grayscale,
-            'BLUR': ImageFilter.GaussianBlur(2),
+            'BLUR': ImageFilter.BLUR,
             'CONTOUR': ImageFilter.CONTOUR,
-            'EMBOSS': ImageFilter.EMBOSS,
             'DETAIL': ImageFilter.DETAIL,
+            'EDGE_ENHANCE': ImageFilter.EDGE_ENHANCE,
             'EDGE_ENHANCE_MORE': ImageFilter.EDGE_ENHANCE_MORE,
+            'EMBOSS': ImageFilter.EMBOSS,
             'FIND_EDGES': ImageFilter.FIND_EDGES,
+            'SMOOTH': ImageFilter.SMOOTH,
             'SMOOTH_MORE': ImageFilter.SMOOTH_MORE,
-            'SHARPEN': ImageFilter.SHARPEN,
+            'SHARPEN': ImageFilter.SHARPEN
         }.get(self.cleaned_data['filters'], None)
