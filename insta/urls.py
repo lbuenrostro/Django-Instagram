efed56372dde67e53a8fca7bd9f8371afb00b242
Lizeth Buenrostro <lbuenrostro@basecampcodingacademy.org>
2018-01-24 20:06:51 -0600

    math a path in urls for a comment image id

--- insta/urls.py
+++ insta/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 
 app_name = 'insta'
 urlpatterns = [
-    path('add/', views.model_form_upload, name="base"),
+    path('add/', views.model_form_upload, name="add"),
     path('feed/', views.show_feed, name="feed"),
     path('filter/glass/<image_id>', views.glass_filter, name='glass'),
     path('filter/grey/<image_id>', views.grey_filter, name='grey'),
@@ -11,4 +11,5 @@ urlpatterns = [
     path('filter/fire/<image_id>', views.fire_filter, name='fire'),
     path('delete/<image_id>', views.remove, name='delete'),
     path('filter/<image_id>', views.Apply_Filter.as_view(), name='filter'),
+    path('comment/<image_id>', views.Comment.as_view(), name='comment'),
 ]
