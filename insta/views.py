3446d8e1d9c6a0b50e9d619a3fcaf4bbb95c3422
Lizeth Buenrostro <lbuenrostro@basecampcodingacademy.org>
2018-01-24 20:08:58 -0600

    In views I made a class for comment were it request for each image_id to have its own comments and are save in the feed

--- insta/views.py
+++ insta/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.conf import settings
 from django.core.files.storage import FileSystemStorage
 from django.http import HttpResponseRedirect, HttpResponse
-from .forms import DocumentForm, FiltersForm
+from .forms import DocumentForm, FiltersForm, CommentForm
 from insta import models
 from PIL import Image, ImageFilter
 from insta.imagefilters import convert_grayscale, broken_glass, rain_fall, fire_all
@@ -25,9 +25,13 @@ def model_form_upload(request):
 
 # example of how to do it filters on view
 def show_feed(request):
+    commentform = CommentForm()
     pictures = [{
         'url': picture.photo.url.replace('insta/static', ''),
-        'id': picture.id
+        'id': picture.id,
+        'description': picture.description,
+        'comment_form': commentform,
+        'comments': picture.comment_set.all()
     } for picture in models.DocumentForm.objects.all()][::-1]
     return render(request, 'insta/feed.html', {'pictures': pictures})
 
@@ -61,7 +65,7 @@ def fire_filter(request, image_id):
 
 
 def remove(request, image_id):
-    models.DocumentForm.objects.get(id=image_id).delete()
+    delete = models.DocumentForm.objects.get(id=image_id).delete()
     return redirect('insta:feed')
 
 
@@ -95,3 +99,14 @@ class Apply_Filter(View):
             image.convert('RGB').filter(f).save(path)
             return redirect('insta:feed')
         return render(request, 'insta/filter-image.html', {'form': form})
+
+
+class Comment(View):
+    def post(self, request, image_id):
+        pic = models.DocumentForm.objects.get(id=image_id)
+        form = CommentForm(pic, request.POST)
+        if form.is_valid():
+            form.saveComment()
+            return redirect('insta:feed')
+        else:
+            return redirect('insta:feed')
